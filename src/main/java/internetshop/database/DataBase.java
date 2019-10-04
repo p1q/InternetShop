121b18f58ef409c7036a900976f25e3be9e0c987
AdminPC\Admin <admin2k7@gmail.com>
2019-10-04 14:02:53 +0300

    Storage deleted

--- src/main/java/internetshop/database/DataBase.java
+++
@@ -1,9 +0,0 @@
-package internetshop.database;
-
-import internetshop.model.Order;
-import java.util.ArrayList;
-import java.util.List;
-
-public class DataBase {
-    public static final List<Order> orders = new ArrayList<>();
-}
